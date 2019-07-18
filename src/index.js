ddd5dd1a8dc97d8d700514f054484f971defeb57
Nana Adjei Manu <claeusdev@outlook.com>
2019-07-18 09:13:13 +0000

    Add prettier config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-const name = "Nana";
-typeof name === "string";
+const name = 'Nana'
+typeof name === 'string'
 
-if (!"serviceWorker" in navigator) {
+if (!('serviceWorker' in navigator)) {
 }
 
-const greeting = "Hello";
-
+const greeting = 'Hello'
 console
   .log(`${greeting} world`)
 
-  [(1, 2, 3)].forEach(x => console.log(x));
+  [(1, 2, 3)].forEach(x => console.log(x))
