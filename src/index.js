072f6ad1524f9e204388970a8ad2cd783f96b327
Nana Adjei Manu <claeusdev@outlook.com>
2019-07-18 09:03:32 +0000

    init with eslint

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const name = "Nana";
+typeof name === "string";
+
+if (!"serviceWorker" in navigator) {
+}
+
+const greeting = "Hello";
+
+console
+  .log(`${greeting} world`)
+
+  [(1, 2, 3)].forEach(x => console.log(x));
